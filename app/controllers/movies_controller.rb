22b6a6ac4e00829e40becea0818622a00a61dd9f
siki2-uiuc <siki2@illinois.edu>
2022-10-12 20:10:27 +0000

    movies functionality done

---
+++ app/controllers/movies_controller.rb
@@ -0,0 +1,18 @@
+class MoviesController < ApplicationController
+
+  def index
+    @list_of_movies = Movie.all
+
+    render({ :template => "movie_templates/index.html.erb"})
+  end
+
+  def movie_details
+    movie_id = params.fetch("id")
+
+    @the_movie = Movie.where( {:id => movie_id }).first
+    @the_director = Director.where({ :id => @the_movie.director_id}).first
+
+    render({ :template => "movie_templates/show.html.erb"})
+  end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -5,5 +5,8 @@ Rails.application.routes.draw do
   get("/directors/eldest", { :controller => "directors", :action => "wisest"})
   get("/directors/youngest", { :controller => "directors", :action => "youngest"})
   get("/directors/:an_id", { :controller => "directors", :action => "director_details"})
+
+  get("/movies", { :controller => "movies", :action => "index"})
+  get("/movies/:id", { :controller => "movies", :action => "movie_details"})
   
 end
